4decb86ea134088d16cec611d56767a4305c9462
scjohnson243 <47642478+scjohnson243@users.noreply.github.com>
2019-03-01 11:49:58 -0600

    Add files via upload

---
+++ BlockingSQL/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BlockingSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_block = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_block
+            // 
+            this.btn_block.Location = new System.Drawing.Point(336, 12);
+            this.btn_block.Name = "btn_block";
+            this.btn_block.Size = new System.Drawing.Size(75, 28);
+            this.btn_block.TabIndex = 0;
+            this.btn_block.Text = "Blocking";
+            this.btn_block.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 94);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_block);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Production Blocking Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_block;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BlockingSQL/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Timers;
+
+namespace BlockingSQL
+{
+
+ 
+    public partial class Form1 : Form
+    {
+        string conString = @"Server=stephenssql\steph;Database=master;Trusted_Connection=True;";
+        string queryString = @"SELECT
+db.name DBName,
+tl.request_session_id,
+wt.blocking_session_id,
+OBJECT_NAME(p.OBJECT_ID) BlockedObjectName,
+tl.resource_type,
+h1.TEXT AS RequestingText,
+h2.TEXT AS BlockingTest,
+tl.request_mode
+FROM sys.dm_tran_locks AS tl
+INNER JOIN sys.databases db ON db.database_id = tl.resource_database_id
+INNER JOIN sys.dm_os_waiting_tasks AS wt ON tl.lock_owner_address = wt.resource_address
+INNER JOIN sys.partitions AS p ON p.hobt_id = tl.resource_associated_entity_id
+INNER JOIN sys.dm_exec_connections ec1 ON ec1.session_id = tl.request_session_id
+INNER JOIN sys.dm_exec_connections ec2 ON ec2.session_id = wt.blocking_session_id
+CROSS APPLY sys.dm_exec_sql_text(ec1.most_recent_sql_handle) AS h1
+CROSS APPLY sys.dm_exec_sql_text(ec2.most_recent_sql_handle) AS h2";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitalizeTimer();
+        }
+
+        private int counter;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+
+           
+            
+        }
+
+        private void InitalizeTimer()
+        {
+            counter = 0;
+            timer1.Interval = 10000;
+            timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            
+
+
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            counter = counter + 1;
+            label1.Text = "Last Ran: " + DateTime.Now.ToString();
+
+            btn_block.BackColor = Color.Yellow;
+
+            SqlConnection dbConn = new SqlConnection(conString);
+            dbConn.Open();
+            SqlCommand queryCommand = new SqlCommand(queryString, dbConn);
+            SqlDataReader querycommandreader = queryCommand.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(querycommandreader);
+            if (dataTable.Rows.Count == 0)
+            {
+                btn_block.BackColor = Color.Green;
+                btn_block.Text = "No Blocking";
+            }
+            else
+            {
+                btn_block.BackColor = Color.Red;
+                btn_block.Text = "BLOCKING";
+                MessageBox.Show("Blocking Detected on the SQL Server");
+            }
+
+            dbConn.Close();
+
+        }
+    }
+}
